ebd0682471983a012d71cf4d3b44f28a9bc6f928
Guilherme Porto <guilherme.porto@lambda3.com.br>
2017-12-03 13:56:02 -0200

    Adicionando Models e Migrations com EF Core Cli

---
+++ DemoDotNetCore.WebApi/Models/BloggingDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace DemoDotNetCore.WebApi.Models
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
--- DemoDotNetCore.WebApi/Startup.cs
+++ DemoDotNetCore.WebApi/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace DemoDotNetCore.WebApi
+﻿namespace DemoDotNetCore.WebApi
 {
+    using Models;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -24,6 +20,11 @@ namespace DemoDotNetCore.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
+            services
+                .AddDbContext<BloggingContext>(
+                    options => options
+                    .UseSqlServer(Configuration.GetConnectionString("DemoDotNetCoreDB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
