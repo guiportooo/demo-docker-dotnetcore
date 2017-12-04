71f4010970071f6252ca89c2a5142347f3772f83
Guilherme Porto <guilherme.porto@lambda3.com.br>
2017-12-04 19:36:57 -0200

    Adicionando arquivos docker

--- DemoDotNetCore.WebApi/Controllers/CommentsController.cs
+++ DemoDotNetCore.WebApi/Controllers/CommentsController.cs
@@ -1,6 +1,9 @@
 ﻿namespace DemoDotNetCore.WebApi.Controllers
 {
+    using MailKit.Net.Smtp;
+    using MailKit.Security;
     using Microsoft.AspNetCore.Mvc;
+    using MimeKit;
     using Models;
     using System.Collections.Generic;
     using System.Linq;
@@ -34,6 +37,23 @@
             }
 
             _bloggingContext.SaveChanges();
+
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("BlogApi", "blogapi@demodocker.com"));
+            message.To.Add(new MailboxAddress("", email));
+            message.Subject = $"New comment from {Comment.Author}!";
+
+            message.Body = new TextPart("plain")
+            {
+                Text = Comment.Content
+            };
+
+            using (var mailClient = new SmtpClient())
+            {
+                await mailClient.ConnectAsync(MAIL_HOST, MAIL_PORT, SecureSocketOptions.None);
+                await mailClient.SendAsync(message);
+                await mailClient.DisconnectAsync(true);
+            }
 
             return CreatedAtRoute("GetComment", new { id = Comment.CommentId }, Comment);
         }
